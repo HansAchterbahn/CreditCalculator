8e8b71a6edd20a8672161523a8287ac32dd82de6
Hans Achterbahn <hans.achterbahn@posteo.de>
2024-03-17 20:36:27 +0100

    improved script: last month in last year, final back paid sum

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     kredit_summe = 475000   # Euro
-    zins = 0.05             # Prozent
-    tilgung = 2800          # Euro
+    zins = 0.0361             # Prozent
+    tilgung = 1668          # Euro
+
+    monate = ["Jan.", "Feb.", "Mär.", "Apr.", "Mai", "Jun.", "Jul.", "Aug.", "Sep.", "Okt.", "Nov.", "Dez."]
 
     restschuld = kredit_summe
     jahr = 0
-
+    monat_letzter = 0
+    tilgung_letzte = 0
     jahre = []
     restschulden = []
 
@@ -15,13 +18,36 @@ if __name__ == '__main__':
         jahre.append(jahr)
         restschulden.append(restschuld)
 
-        restschuld = (restschuld - 12 * tilgung) * (1+zins)
+        if restschuld == 0:
+            break
+
+        for i in range(12):
+            restschuld = restschuld - tilgung
+            if restschuld < 0:
+                tilgung_letzte = tilgung - restschuld
+                restschuld = 0
+                monat_letzter = int(i)
+                break
+
+        restschuld = restschuld * (1+zins)
         jahr = jahr + 1
 
-        print("Jahr", jahr, "Restschuld:", restschuld)
+        print("Jahr", jahr, "Restschuld:", round(restschuld))
+
+
 
         if jahr > 100:
             break
+
+
+print()
+print("Letztes Jahr:", jahr)
+print("Letzter Monat:", str(monat_letzter))
+
+gesamt_summe = ((jahr-1) * 12 + monat_letzter) * tilgung + tilgung_letzte
+print("Summe Gesamt:", round(gesamt_summe))
+print("Zurückgezahlt:", round(gesamt_summe/kredit_summe*100), "%")
+
 
 plt.plot(jahre,restschulden)
 plt.show()
