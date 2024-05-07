273e6aaf5c1a6cb3cb2bd58891fd4d2cdc8c2b88
Hans Achterbahn <hans.achterbahn@posteo.de>
2024-05-07 22:07:01 +0200

    [feature] every year of the credit can now  have different conditions

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     # Eingangsgrößen
 
     preis_haus                = 399000
-    preis_sanierung_haupthaus = 300000  # über den Daumen gepeilt mit Internet Tool
-    preis_moebel_haupthaus    =  50000
-    preis_sanierung_saal      = 500000  # über den Daumen gepeilt mit Internet Tool
+    preis_sanierung_haupthaus = 50000 #200000 # nur die Außendämmung, Wände und Bäder #300000 # über den Daumen gepeilt mit Internet Tool
+    preis_moebel_haupthaus    = 0
+    preis_sanierung_saal      = 250000  # über den Daumen gepeilt mit Internet Tool
     preis_moebel_saal         = 0
-    preis_acker               = 0
-    preis_wiese               = 0
+    preis_acker               = 20000
+    preis_wiese               = 5000
 
-    kredit_summe    = preis_haus + \
-        preis_sanierung_haupthaus + \
-        preis_moebel_haupthaus + \
-        preis_sanierung_saal + \
-        preis_moebel_saal + \
-        preis_wiese + \
-        preis_acker
+    einzelposten = [
+        preis_haus,
+        preis_sanierung_haupthaus,
+        #preis_moebel_haupthaus,
+        #preis_sanierung_saal,
+        #preis_moebel_saal,
+        #preis_wiese,
+        #preis_acker
+    ]
 
-    zinssatz        = 0.04 #0.0371        # Prozent
-    monatliche_rate = 6000          # Euro
+    kredit_summe = sum(einzelposten)
+
+    jahres_modalitaeten = {
+        0:  {'zinssatz': 0.04, 'monatliche_rate': 2200, 'sondertilgung': 0},
+#        0: {'zinssatz': 0.0219, 'monatliche_rate': 1800, 'sondertilgung': 0},
+#        5:  {'zinssatz': 0.04, 'monatliche_rate': 6000, 'sondertilgung': 30000},
+#        6:  {'zinssatz': 0.04, 'monatliche_rate': 6000, 'sondertilgung': 0},
+#        10: {'zinssatz': 0.03, 'monatliche_rate': 6000, 'sondertilgung': 0}
+    }
+
+    zinssatz        = jahres_modalitaeten[0]['zinssatz']
+    monatliche_rate = jahres_modalitaeten[0]['monatliche_rate']
+    sondertilgung   = jahres_modalitaeten[0]['sondertilgung']
 
     # Init der Variablen
     monate = ["Jan.", "Feb.", "Mär.", "Apr.", "Mai", "Jun.", "Jul.", "Aug.", "Sep.", "Okt.", "Nov.", "Dez."]
@@ -32,6 +46,7 @@ if __name__ == '__main__':
     restschulden = []
     zinsen = []
     tilgungen = []
+    sondertilgungen = []
 
     # Restschuld wird auf die Kreditsumme gesetzt
     jahres_zins = kredit_summe * zinssatz
@@ -40,11 +55,21 @@ if __name__ == '__main__':
 
     # Durchlaufe das nächste Jahr bis die Restschuld beglichen ist
     while restschuld > 0:
-        # Listen für Grafische darstellung befüllen → Restschuld in Euro über die Laufzeit in Jahren
-        jahre.append(jahr)
-        restschulden.append(restschuld)
-        zinsen.append(jahres_zins)
-        tilgungen.append(tilgung)
+        # Prüfen, ob sich für das aktuelle Jahr Änderungen ergeben haben
+        if jahr in jahres_modalitaeten:
+            zinssatz        = jahres_modalitaeten[jahr]['zinssatz']
+            monatliche_rate = jahres_modalitaeten[jahr]['monatliche_rate']
+            sondertilgung   = jahres_modalitaeten[jahr]['sondertilgung']
+
+        # Sondertilgung von der Restschuld abziehen
+        restschuld = restschuld - sondertilgung
+        sondertilgungen.append(sondertilgung)
+
+        # Daten für Graphen aufnehemen
+        jahre.append(jahr)                  # aktuelles Laufzeitjahr
+        restschulden.append(restschuld)     # Restschuld in Euro über die Laufzeit in Jahren
+        zinsen.append(jahres_zins)          # aktuell zu zahlende Jahreszinsen
+        tilgungen.append(tilgung)           # aktuell zu zahlende Jahrestilgung
 
         # Monateweise abrechnung der Tilgung, um den letzten Monat exakt bestimmen zu können
         for i in range(12):
@@ -53,7 +78,7 @@ if __name__ == '__main__':
             if restschuld < 0:
                 monatliche_rate_letzte = monatliche_rate + restschuld
                 restschuld = 0
-                monat_letzter = int(i)
+                monat_letzter = int(i+1)
                 break
 
         # Berechnung der Restschuld für das Folgejahr und Jahreszahl erhöhen
@@ -76,7 +101,8 @@ print("Letztes Jahr:", jahr)
 print("Letzter Monat:", str(monat_letzter))
 
 # Berechnung und Ausgabe der insgesamt für den Kredit gezahlten Summe + Relation zur Kreditsumme
-rueckzahlung_vollstaendig = ((jahr - 1) * 12 + monat_letzter) * monatliche_rate + monatliche_rate_letzte
+sondertilgungen_summe = sum(sondertilgungen)
+rueckzahlung_vollstaendig = ((jahr - 1) * 12 + monat_letzter) * monatliche_rate + monatliche_rate_letzte + sondertilgungen_summe
 print()
 print("     Kredit:", kredit_summe, "€")
 print("Rückzahlung:", round(rueckzahlung_vollstaendig), "€")
@@ -86,18 +112,18 @@ print("Rück.  Rel.:", round(rueckzahlung_vollstaendig / kredit_summe * 100), "%
 
 plt.figure(1)
 plt.subplot(1,2,1)
-plt.plot(jahre,restschulden, label='Restschulden')
+plt.plot(jahre,np.array(restschulden)/1000, label='Restschulden')
 plt.legend()
 plt.xlabel("Jahre")
-plt.ylabel("Euro")
+plt.ylabel("Tausend Euro")
 plt.grid(True)
 
 plt.subplot(1,2,2)
-plt.plot(jahre,tilgungen, label='Tilgung')
-plt.plot(jahre,zinsen, label='Zinsen')
+plt.plot(jahre,np.array(tilgungen)/1000, label='Tilgung')
+plt.plot(jahre,np.array(zinsen)/1000, label='Zinsen')
 plt.legend()
 plt.xlabel("Jahre")
-plt.ylabel("Euro")
+#plt.ylabel("Tausend Euro")
 plt.grid(True)
 
 plt.show()
